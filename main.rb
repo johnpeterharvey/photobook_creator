670d8928093054fb2b1656d3d83cbd28a417f33f
John Peter Harvey <john@johnpeterharvey.com>
2020-12-21 21:52:42 +0000

    Bit more working, can generate a pdf now

--- main.rb
+++ main.rb
@@ -1,24 +1,36 @@
 require 'prawn'
 require 'json'
 require 'date'
+require 'pathname'
 
+page_data = {}
 feed_file = File.open 'export/feed.json'
 data = JSON.load feed_file
 data['items'].each do |item|
   item_date = DateTime.parse(item['date_published']).strftime('%Y-%m-%d')
   item_description = /<p>([^<]+)/.match(item['content_html'])[1].gsub("\n", ', ')
   image_source = /src=\"([^\"]+)/.match(item['content_html'])[1]
-  p "#{item_date}: #{item_description}"
+  page_data[item_date] = {:description => item_description, :source => image_source}
 end
+page_data = page_data.sort_by { |date, values| date }
 
 Prawn::Document.generate("out.pdf", page_size: [693, 594], :margin => [0,0,0,0]) do
-  fill_color '000000'
-  fill_rectangle [bounds.left, bounds.top], bounds.right, bounds.top
+  page_data.each do |date, values|
+    start_new_page
 
-  image = "./example2.jpg"
-  image image, :at => [0, bounds.top], :position => :center, :vposition => :center, :fit => [693, 540]
+    # Formatting
+    fill_color '000000'
+    fill_rectangle [bounds.left, bounds.top], bounds.right, bounds.top
+    font('/System/Library/Fonts/HelveticaNeue.ttc', size: 11, style: 'Light')
+    fill_color 'FFFFFF'
 
-  font('/System/Library/Fonts/HelveticaNeue.ttc', size: 11, style: 'Light')
-  fill_color 'FFFFFF'
-  text_box '2020-03-03: Test Image', :at => [0, 40], :width => bounds.right, align: :center
+    # Contents
+    image_file = Pathname.new("export/#{values[:source]}")
+    if image_file.exist?
+      image image_file.realpath, :at => [0, bounds.top], :position => :center, :vposition => :center, :fit => [693, 540]
+    else
+      puts "Problem image #{date} - #{image_file}"
+    end
+    text_box "#{date}: #{values[:description]}", :at => [0, 40], :width => bounds.right, align: :center
+  end
 end
