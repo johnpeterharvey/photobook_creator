5f979ee05ed49e8aa4f9b99b75d0f9ea0f3c90d2
John Peter Harvey <john@johnpeterharvey.com>
2021-03-11 22:52:31 +0000

    Tidying, and improved formatting

--- main.rb
+++ main.rb
@@ -4,9 +4,11 @@ require 'json'
 require 'date'
 
 current_year = 2020
+
 page_width = 693
 page_height = 594
-height_subtract = 60
+height_subtract = 38
+text_position = 30
 
 page_data = {}
 feed_file = File.open 'export/feed.json'
@@ -32,7 +34,7 @@ Prawn::Document.generate("out.pdf", page_size: [page_width, page_height], :margi
     start_new_page
 
     month.each { |date|
-      p date
+      puts date
       # Formatting
       fill_color '000000'
       fill_rectangle [bounds.left, bounds.top], bounds.right, bounds.top
@@ -41,19 +43,23 @@ Prawn::Document.generate("out.pdf", page_size: [page_width, page_height], :margi
 
       # Contents
       text "\n"
-      image "export/#{page_data[date][:source]}", :position => :center, :fit => [page_width, page_height - height_subtract]
-      text_box "#{date}: #{page_data[date][:description]}", :at => [0, 40], :width => bounds.right, align: :center
+      # Image
+      bounding_box([0, page_height], width: page_width, height: page_height - height_subtract) do
+        image "export/#{page_data[date][:source]}", :position => :center, :vposition => :center, :fit => [page_width, page_height - height_subtract]
+      end
+      # Image description
+      text_box "#{date}: #{page_data[date][:description]}", :at => [0, text_position], :width => bounds.right, align: :center
     
       start_new_page
     }
   end
   # Final page
   if (dates.length % 2) then
-    p "Odd number of pages, adding final blank page"
+    puts "Odd number of pages, adding final blank page"
     fill_color '000000'
     fill_rectangle [bounds.left, bounds.top], bounds.right, bounds.top
   else
-    p "Even number of pages, removing extraneous extra"
+    puts "Even number of pages, removing extraneous extra"
     delete_page(-1)
   end
 end
