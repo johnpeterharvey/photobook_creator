8490d62dd87fc81ce73ad3ce4cd4b9dfda347946
John Peter Harvey <john@johnpeterharvey.com>
2022-01-31 16:53:22 +0000

    Handle days when nothing was posted

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require 'prawn/emoji'
 require 'json'
 require 'date'
 
-current_year = 2020
+current_year = 2021
 
 page_width = 693
 page_height = 594
@@ -13,12 +13,21 @@ text_position = 30
 page_data = {}
 feed_file = File.open 'export/feed.json'
 data = JSON.load feed_file
+
+# Init the page data with no image blanks
+(Date.new(current_year, 1, 1)..Date.new(current_year, 12, 31)).to_a.each do |date|
+  page_data[date.strftime('%Y-%m-%d')] = {:description => "Nothing Posted", :source => "no_image.png"}
+end
+
+# For all the items posted, iterate through
 data['items'].each do |item|
   item_date = DateTime.parse(item['date_published']).strftime('%Y-%m-%d')
   item_description = /<p>([^<]+)/.match(item['content_html'])[1].gsub("\n", ', ')
   image_source = /src=\"([^\"]+)/.match(item['content_html'])[1]
   page_data[item_date] = {:description => item_description, :source => image_source}
 end
+
+# Filter to only the required year, and slice into an array of arrays by month
 dates = page_data.keys.sort.select {|date| /^#{current_year}.*/.match(date)}.slice_before(/.*01$/).to_a
 
 Prawn::Document.generate("out.pdf", page_size: [page_width, page_height], :margin => [0,0,0,0]) do
@@ -43,13 +52,14 @@ Prawn::Document.generate("out.pdf", page_size: [page_width, page_height], :margi
 
       # Contents
       text "\n"
+
       # Image
       bounding_box([0, page_height], width: page_width, height: page_height - height_subtract) do
         image "export/#{page_data[date][:source]}", :position => :center, :vposition => :center, :fit => [page_width, page_height - height_subtract]
       end
       # Image description
       text_box "#{date}: #{page_data[date][:description]}", :at => [0, text_position], :width => bounds.right, align: :center
-    
+
       start_new_page
     }
   end
